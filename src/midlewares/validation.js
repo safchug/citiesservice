17758bfbd5966d887b36c0f48a061c5c096fd71b
Vasyl Savchuk <safchug@gmail.com>
2020-11-25 19:56:38 +0200

    changed status code when validation fails

--- src/midlewares/validation.js
+++ src/midlewares/validation.js
@@ -2,14 +2,16 @@ const Joi = require('joi');
 
 const validation = (schema, property) => {
     return (req, res, next) => {
-        const { error } = schema.validate(req[property]);
+        const { error } = schema.validate(req[property], { abortEarly: false });
         const valid = error == null;
         if (valid) { next(); }
         else {
             const { details } = error;
+            console.log('error', error)
+            //console.log('details', details);
             const message = details.map(i => i.message).join(',')
             console.log("error", message);
-            res.status(422).json({ error: message })
+            res.status(400).json({ message })
         }
     }
 }
