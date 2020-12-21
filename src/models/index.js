34590cf721d70e1215db362c8dbf67a893f63b6a
Vasyl Savchuk <safchug@gmail.com>
2020-12-21 16:26:58 +0200

    changed uri to mongodb

--- src/models/index.js
+++ src/models/index.js
@@ -6,8 +6,7 @@ const password = process.env.db_conection_password;
 const user = process.env.db_conection_user;
 const auth = process.env.auth_source;
 
-//var uri = `mongodb://${user}:${password}@127.0.0.1:27017/?authSource=${auth}`;
-var uri = "mongodb://localhost:27017/"
+var uri = "mongodb://mongo:27017/"
 var dbname = "citiesService";
 
 var db;
