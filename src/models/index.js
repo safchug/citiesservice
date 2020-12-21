57d07d5211afcc899b5b0c54c2df417a8d42ecf3
Vasyl Savchuk <safchug@gmail.com>
2020-12-21 16:37:37 +0200

    changed uri for the mongodb database

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,6 @@
 require('dotenv/config');
 
 var MongoClient = require('mongodb').MongoClient;
-
-const password = process.env.db_conection_password;
-const user = process.env.db_conection_user;
-const auth = process.env.auth_source;
-
 var uri = "mongodb://mongo:27017/"
 var dbname = "citiesService";
 
